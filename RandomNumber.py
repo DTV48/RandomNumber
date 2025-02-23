c36107e2c1ce3e28d5cd8f92362e6229c947b6e7
DTV48 <mygao2012@outlook.com>
2025-02-23 19:07:59 +0800

    1

---
+++ RandomNumber.py
@@ -0,0 +1,7 @@
+import random
+LowNum=input("low")
+MaxNum=input("max")
+while True:
+    print(random.randint(int(LowNum),int(MaxNum)))
+    print(random.randint(int(LowNum),int(MaxNum)))
+    input("press enter to continue")
